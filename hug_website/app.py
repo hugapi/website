ab0b2dfda9e6c81e0b6a1285ef765ea4a1edcecd
Timothy Crosley <timothy.crosley@gmail.com>
2016-03-11 07:55:55 -0800

    Initial home page

--- hug_website/app.py
+++ hug_website/app.py
@@ -14,12 +14,42 @@ def static_files():
     return ('hug_website/static', )
 
 
-@app.transform(html(controllers.frame), urls=('/', '/website/{page_name}'))
+@app.transform(html(controllers.frame), urls=('/', '/website/{page_name}'), on_invalid=False)
 def root(page_name:hug.types.one_of(('home', ))='home'):
     return {'label': 'hug', 'version': hug.__version__,
-            'content': globals()[page_name]()}
+            'content': globals()[page_name](), 'page': page_name}
 
 
 @app.transform(html(controllers.home))
 def home():
-    return [{'text': 'hi!'}]
+    return {'slogan': 'Embrace the APIs of the future',
+            'introduction': 'Drastically simplify API development over multiple interfaces. With hug you design '
+                            'and develop your API once and then can easily expose it however your clients need to '
+                            'consume it. Be it locally, over HTTP, or through the command line - hug is the fastest '
+                            'and most modern way to create APIs on Python3.',
+            'example_header': 'Obvious. Clean. Radically simple.',
+            'performance_header': 'Unparalleled performance',
+            'performance_description': 'hug has been built from the ground up with performance in mind. It is built '
+                                       'to consume resources only when necessary and is then compiled with Cython '
+                                       'to achieve amazing performance. As a result, hug consistently benchmarks as '
+                                       'one of the fastest Python frameworks and without question takes the crown '
+                                       'as the fastest high-level framwork for Python 3.',
+            'versioning_header': 'Built in version management',
+            'versioning_description': 'hug makes it easy to expose multiple versions of your API. With hug you can '
+                                      'simply specify what version or range of versions an endpoint supports and then '
+                                      'automatically have that enforced and communicated to your API\'s users.',
+            'documentation_header': 'Automatic documentation',
+            'documentation_description': 'Python makes it easy to document your APIs well using doc strings and types '
+                                         'annotations. hug uses this information to automatically generate '
+                                         'documentation for users of your API so you don\'t have to.',
+            'annotation_header': 'Annotation powered validation',
+            'annotation_description': 'hug leverages Python 3 type annotations to enable simple per argument '
+                                      'validation and transformation. This leads to explicit and easy to follow '
+                                      'endpoints.',
+            'reuse_header': 'Write once. Use everywhere.',
+            'reuse_description': 'With hug your API and business logic is cleanly separated from the interface your '
+                                 'exposing it on, which means you can safely expose it over HTTP, CLI, and Python in '
+                                 'one full swoop.',
+            'get_started_header': 'What are you waiting for?',
+            'get_started_description': 'Start writing world class APIs on top of Python 3 in no time. Use hug {0} to '
+                                       'radically simplify your code base.'.format(hug.__version__)}
--- hug_website/controllers.py
+++ hug_website/controllers.py
@@ -4,9 +4,26 @@ from blox.text import Text
 
 def frame(data, template=filename('hug_website/views/frame.shpaml')):
     ui = template()
+    ui.version.text = data['version']
+    ui.main_content(globals()[data['page']](data['content']))
     return ui
 
 
 def home(data, template=filename('hug_website/views/home.shpaml')):
     ui = template()
+    ui.slogan.text = data['slogan']
+    ui.introduction.text = data['introduction']
+    ui.example_header.text = data['example_header']
+    ui.performance_header.text = data['performance_header']
+    ui.performance_description.text = data['performance_description']
+    ui.versioning_header.text = data['versioning_header']
+    ui.versioning_description.text = data['versioning_description']
+    ui.documentation_header.text = data['documentation_header']
+    ui.documentation_description.text = data['documentation_description']
+    ui.annotation_header.text = data['annotation_header']
+    ui.annotation_description.text = data['annotation_description']
+    ui.reuse_header.text = data['reuse_header']
+    ui.reuse_description.text = data['reuse_description']
+    ui.get_started_header.text = data['get_started_header']
+    ui.get_started_description.text = data['get_started_description']
     return ui
