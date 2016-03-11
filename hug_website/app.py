34cc7cbc872b2c362e123821750126fbf9c3ba9a
Timothy Crosley <timothy.crosley@gmail.com>
2016-03-10 17:42:31 -0800

    Add static files

--- hug_website/app.py
+++ hug_website/app.py
@@ -1,12 +1,25 @@
 """hug's website: hug.rest"""
+from functools import partial
+
 import hug
+
+from hug_website import controllers
+
+app = hug.get(output=hug.output_format.suffix({'/js': hug.output_format.json}, hug.output_format.html)).suffixes('/js')
+html = partial(hug.transform.suffix, {'/js': None})
 
 
 @hug.static('/static', cache=True)
 def static_files():
-    return ('static', )
+    return ('hug_website/static', )
 
 
-@hug.get('/')
+@app.transform(html(controllers.frame), urls=('/', '/website/{page_name}'))
+def root(page_name:hug.types.one_of(('home', ))='home'):
+    return {'label': 'hug', 'version': hug.__version__,
+            'content': globals()[page_name]()}
+
+
+@app.transform(html(controllers.home))
 def home():
-    return "Website"
+    return [{'text': 'hi!'}]
