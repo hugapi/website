5522178b9bba47e2e47375e3f24bb1513c2a7f17
Timothy Crosley <timothy.crosley@gmail.com>
2016-03-17 02:08:08 -0700

    Use.html

--- build.sh
+++ build.sh
@@ -14,18 +14,18 @@ cp ../hug_website/hug_website/static/images -rf static
 git add static
 
 wget $SERVER -O index.html
-wget "$SERVER"website/quickstart -O website/quickstart
+wget "$SERVER"website/quickstart -O website/quickstart.html
 wget "$SERVER"website/learn -O website/learn/index.html
-wget "$SERVER"website/contribute -O website/contribute
-wget "$SERVER"website/discuss -O website/discuss
-wget "$SERVER"website/home -O website/home
+wget "$SERVER"website/contribute -O website/contribute.html
+wget "$SERVER"website/discuss -O website/discuss.html
+wget "$SERVER"website/home -O website/home.html
 
-wget "$SERVER"website/learn/architecture -O website/learn/architecture
-wget "$SERVER"website/learn/routing -O website/learn/routing
-wget "$SERVER"website/learn/type_annotation -O website/learn/type_annotation
-wget "$SERVER"website/learn/directives -O website/learn/directives
-wget "$SERVER"website/learn/output_formats -O website/learn/output_formats
-wget "$SERVER"website/learn/extending -O website/learn/extending
+wget "$SERVER"website/learn/architecture -O website/learn/architecture.html
+wget "$SERVER"website/learn/routing -O website/learn/routing.html
+wget "$SERVER"website/learn/type_annotation -O website/learn/type_annotation.html
+wget "$SERVER"website/learn/directives -O website/learn/directives.html
+wget "$SERVER"website/learn/output_formats -O website/learn/output_formats.html
+wget "$SERVER"website/learn/extending -O website/learn/extending.html
 
 wget $SERVER/404 -O 404.html --content-on-error
 
