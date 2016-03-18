383d24a71be8ff40c1b5f0932747703491eb7d85
Cory Taylor <coryandrewtaylor@gmail.com>
2016-03-18 16:35:48 -0500

    Proofread
    Proofread quickstart() and part of home()

--- hug_website/app.py
+++ hug_website/app.py
@@ -67,47 +67,47 @@ def not_found():
 def quickstart():
     return {'install_header': 'Installing hug',
             'install_description': 'The first step to get started is to install hug. hug has very minimal base system '
-                                   'requirements - which is essentially a local installation of Python3.3+ optionally '
-                                   'inside a virtualenv. Additionally, pip is required, but this should be included '
-                                   'with most Python3 installations by default. Once the base system is in good shape, '
-                                   'run the following command to install the latest version of hug:',
+                                   'requirements - a local installation of Python3.3+, optionally inside a virtualenv. '
+                                   'Additionally, pip is required, but this should be included with most Python3 '
+                                   'installations by default. Once the base system is in good shape, run the following '
+                                   'command to install the latest version of hug:',
             'first_header': 'First hug API',
-            'first_description': 'To start off we are going to make a simple API that has local access only, but '
-                                 'demonstrates a couple of basic hug features: annotation based validation and '
+            'first_description': 'To start off, we are going to make a simple API with local access only, but which '
+                                 'demonstrates a couple of basic hug features: annotation-based validation and '
                                  'directives. Our first API will simply return a happy birthday message to the '
-                                 'user of the API along with the time it took to generate the message:',
+                                 'user, along with the time it took to generate the message:',
             'first_explaination': "In this example: hug's built-in type annotation automatically validates and "
                                   "converts incoming inputs while hug's directives automatically replace the hug_timer "
                                   'argument with a HugTimer object that keeps track of how long the function has been '
-                                  'running. hug type annotations are at their core simply functions or objects which '
-                                  'take a value as input, cast that value as something (raising on errors) and then '
-                                  'return it. As a result of this, most built-in Python cast functions (int, str, etc) '
+                                  'running. hug type annotations are, at their core, simply functions or objects which '
+                                  'take a value as input, cast that value as something (raising on errors), and then '
+                                  'return it. As a result of this, most built-in Python cast functions (int, str, etc.) '
                                   'are valid annotations in hug out of the box. You can also use Marshmallow schemas '
                                   'and types as hug type annotations without modification.',
             'http_header': 'Exposing our API as an HTTP micro-service',
-            'http_description': 'To expose our API over HTTP all we need to do is apply a hug HTTP route decorator to '
-                                'the function in addition to the local decorator. hug includes convience decorators '
-                                'for all common HTTP methods (GET, POST, PUT, etc), in this case we will simply apply '
-                                'a get decorator to specify it should return on an HTTP GET request. We will also '
-                                'supply an example set of parameters to lead our users in the correct direction:',
+            'http_description': 'To expose our API over HTTP, all we need to do is apply a hug HTTP route decorator to '
+                                'the function, in addition to the local decorator. hug includes convience decorators '
+                                'for all common HTTP methods (GET, POST, PUT, etc). In this case we will apply a get '
+                                'decorator to specify that the function should return on an HTTP GET request. We will '
+                                'also supply an example set of parameters to lead our users in the correct direction:',
             'cli_header': 'Enabling command line interaction',
-            'cli_description': 'What if we want to allow users to interact with our API from the command line as well? '
+            'cli_description': 'What if we want to allow users to interact with our API from the command line, as well? '
                                "No problem! All that's necessary is adding a hug.cli route decorator to our API "
                                'function:',
             'wsgi_header': 'Final step: Production HTTP use',
             'wsgi_description': "Finally, it's important to note that it's generally never a good idea to use a "
                                 "development server (like hug's, Flask's, etc.) directly in production. Instead, "
-                                'a WSGI compatible server such as uwsgi or Gunicorn is recommended. Every hug API that '
-                                'contains an http endpoint, exposes a `__hug_wsgi__` WSGI compatible API automatically '
+                                'a WSGI-compatible server (such as uwsgi or Gunicorn) is recommended. Every hug API that '
+                                'contains an http endpoint automatically exposes a `__hug_wsgi__` WSGI-compatible API '
                                 '- making integration of our above example a breeze:'}
 
 
 @app.transform(html(controllers.home))
 def home():
     return {'slogan': 'Embrace the APIs of the future',
-            'introduction': 'Drastically simplify API development over multiple interfaces. With hug you design '
-                            'and develop your API once and then can easily expose it however your clients need to '
-                            'consume it. Be it locally, over HTTP, or through the command line - hug is the fastest '
+            'introduction': 'Drastically simplify API development over multiple interfaces. With hug, design '
+                            'and develop your API once, then expose it however your clients need to consume it. '
+                            'Be it locally, over HTTP, or through the command line - hug is the fastest '
                             'and most modern way to create APIs on Python3.',
             'example_header': 'Obvious. Clean. Radically simple.',
             'performance_header': 'Unparalleled performance',
